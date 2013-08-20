8514ba52b934758c874ab8cfbb19433c198a36ec
Dan Guo <danguo1113@gmail.com>
2013-08-19 21:48:53 -0700

    Added a basic cron job, is not populating database yet;

--- cron_for_download.py
+++ cron_for_download.py
@@ -2,18 +2,21 @@ from datetime import datetime
 from apscheduler.scheduler import Scheduler
 import time
 import logging
+import download_stock_data
+
 
 logging.basicConfig()
 
-def download_data():
-    curr_dir = '/Users/danguo/Desktop/model/'
-    file = open(curr_dir + 'output.txt','a')
-    file.write("Downloading\n")
 
+
+def download_data():
+    stock_info_dict = download_stock_data.download_stock_data()
+    #Should eventually populate a SQL database
+    print stock_info_dict
 
 
 if __name__ == '__main__':
     sched = Scheduler()
     sched.daemonic = False
     sched.start()
-    sched.add_interval_job(download_data, seconds = 2)
+    sched.add_interval_job(lambda: download_data(), hours=24)
